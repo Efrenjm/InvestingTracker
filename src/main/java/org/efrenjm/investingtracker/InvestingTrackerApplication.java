61c2c722713bbd01ad221915dec30da17aea9865
Efren Jimenez <efrenjimenez_96@hotmail.com>
2024-11-10 20:43:53 -0600

    first commit

---
+++ src/main/java/org/efrenjm/investingtracker/InvestingTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.efrenjm.investingtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestingTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestingTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/efrenjm/investingtracker/InvestingTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.efrenjm.investingtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestingTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
